08cce5c3423002314805027131ed3a7ba3862ed1
Bart <bart_tacken@hotmail.com>
2021-12-22 20:18:51 +0100

    Update Install-module.ps1

--- Install-module.ps1
+++ Install-module.ps1
@@ -2,3 +2,6 @@ $installedModules = Get-InstalledModule
 if ($installedModules.Name -notmatch "Az") {
     Install-Module Az -Force -AllowClobber
 }
+
+# Connect-AzAccount
+# Select-AzSubscription -SubscriptionId SUBSCRIPTION ID | Set-AzContext
